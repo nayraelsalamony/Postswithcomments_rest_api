29d02c7cf89705fd60f01e3b5c2d43e6c0c9e182
nayra <nayraelsalamoney@gmail.com>
2022-05-20 07:03:18 +0200

    edit  article

--- routes/routes.js
+++ routes/routes.js
@@ -52,3 +52,21 @@ router.delete('/delete/:id', async (req, res) => {
         res.status(400).json({ message: error.message })
     }
 })
+
+// edit article
+router.patch('/edit/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
