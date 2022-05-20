43f6e0aa610ac514def714681816fad65e3cf7e7
nayra <nayraelsalamoney@gmail.com>
2022-05-20 06:41:34 +0200

    delete  article

--- routes/routes.js
+++ routes/routes.js
@@ -41,3 +41,14 @@ router.get('/getone/:id', async (req, res) => {
     }
 })
 
+// delete 
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
